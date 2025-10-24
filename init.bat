61025e1abf0c6497affae1702c88f828e67c5566
Ian Pang <ian.eng10@gmail.com>
2025-10-24 02:23:26 -0600

    feat(inicializador): script bat para crear e iniciar todos los contenedores

---
+++ init.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+echo ========================================
+echo   Iniciando los Docker Compose indicados
+echo ========================================
+echo.
+
+set folders=airflow postgres-analitycs mlflow
+
+for %%d in (%folders%) do (
+    echo Ejecutando en %%d ...
+    pushd %%d
+    docker compose up --build -d
+    if %errorlevel% neq 0 (
+        echo Error ejecutando docker compose en %%d
+    )
+    popd
+    echo.
+)
+
+echo ========================================
+echo   Todos los servicios han sido levantados
+echo ========================================
+pause
